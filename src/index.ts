2c2869bb024bc651c2d3ade887d9a22659cc9042
Gabriel Marcos <gabrielmareia@hotmail.com>
2018-07-28 15:53:02 -0300

    Fix review by Vitor Luiz

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as http from 'http'
 
 import app from './app'
 import db from './models'
-import { normalizePort, onError, onListening } from './utils/utils'
+import { normalizePort, onError, onListening, handleError } from './utils/utils'
 
 const server = http.createServer(app)
 const port = normalizePort(process.env.port || 3000)
@@ -13,4 +13,4 @@ db.sequelize.sync()
     server.listen(port)
     server.on('error', onError(server))
     server.on('listening', onListening(server))    
-  })
+  }).catch(handleError)
